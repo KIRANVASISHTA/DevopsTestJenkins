705511600ec4b780abee32393763693fe47495b2
KIRANVASISHTA <kiranvasishta1@gmail.com>
2023-07-30 10:34:27 +0530

    Create test_jenkinsExample.py

---
+++ test_jenkinsExample.py
@@ -0,0 +1,13 @@
+from test import *
+
+def test_add():
+  assert add(3,5) == 8
+
+def test_subtract():
+  assert subtract(4,2) == 2
+
+def test_mul():
+  assert mul(2,3) == 6
+
+def test_div():
+  assert div(3,1) == 3
